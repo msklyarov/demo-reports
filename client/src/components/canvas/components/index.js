5d1b46d1950a8960244117f1fd83a77ec01218bd
Maxim Sklyarov <sklyarov.ma@gmail.com>
2020-03-19 17:14:08 +0300

    update

--- client/src/components/canvas/components/index.js
+++ client/src/components/canvas/components/index.js
@@ -1,28 +1,10 @@
 import React from 'react';
 import Box from '../../box';
 
-// export default ({ widgets }) => (
-//   <>
-//     {widgets.map((item, i) => (
-//       <Box key={i} data={item} />
-//     ))}
-//   </>
-// );
-
-class Canvas extends React.Component {
-  componentDidMount() {
-    this.forceUpdate();
-  }
-
-  render() {
-    return (
-      <>
-        {this.props.widgets.map((item, i) => (
-          <Box key={i} data={item} />
-        ))}
-      </>
-    );
-  }
-}
-
-export default Canvas;
+export default ({ widgets }) => (
+  <>
+    {widgets.map((item, i) => (
+      <Box key={i} data={item} />
+    ))}
+  </>
+);
