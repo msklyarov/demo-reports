5117a25110ffb3a0c52215d9c86d08c68e8443db
Maxim Sklyarov <sklyarov.ma@gmail.com>
2020-03-19 13:04:35 +0300

    uuid remove

--- client/src/modules/widgets/reducers/widgets.js
+++ client/src/modules/widgets/reducers/widgets.js
@@ -4,7 +4,18 @@ import uuidv4 from 'uuid/v4';
 export default (state = [], action) => {
   switch (action.type) {
     case ActionTypes.WIDGET_CREATE:
-      return [...state, { id: uuidv4(), name: action.payload }];
+      return [
+        ...state,
+        {
+          id: (
+            Date.now().toString(36) +
+            Math.random()
+              .toString(36)
+              .substr(2, 5)
+          ).toUpperCase(),
+          name: action.payload,
+        },
+      ];
 
     case ActionTypes.WIDGET_DELETE:
       return state.filter(item => item.id != action.payload);
